c87c3294cbbae668e65582fba640594e87b01574
Nathaniel Griswold <nategriswold@gmail.com>
2021-08-10 17:21:51 -0500

    compose overlays

--- lib.nix
+++ lib.nix
@@ -9,6 +9,10 @@ let
   forDefaultSystems = forSystems defaultSystems;
   forAllSystems = forSystems allSystems;
 
+  # `nix flake check` is picky about overlay function arg names actually being final and prev
+  composeOverlays = e1: e2: final: prev: lib.composeExtensions e1 e2;
+  composeManyOverlays = lib.foldr (x: y: composeOverlays x y);
+
 in {
   inherit defaultSystems allSystems numtide forSystems forDefaultSystems forAllSystems;
 }
